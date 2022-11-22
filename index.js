465eca43eb2ecace4790454bbe8ea5e5efe7313c
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-11-22 22:47:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const message = ["surprise"];
+function writeCards(names, message){
+    let thankyouMessages = [];
+    
+    for(let i = 0; i < names.length; i++){
+        thankyouMessages.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    }
+    return thankyouMessages;
+}
+
+//describe countDown function which invokes console.log once for each number, counting down from the number provided to zero
+function countDown(y){
+    for (let i = y; i > -1; i--){
+        console.log(i);
+    }
+}
+countDown(10)
